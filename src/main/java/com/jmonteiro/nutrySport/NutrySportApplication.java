cac38c00ae4f18ddb2c3fc0326d4f0ee7e7b100a
João MONTEIRO <jmonteiro@apsdigit.lan>
2023-02-28 16:48:55 +0000

    repository created in git hub

---
+++ src/main/java/com/jmonteiro/nutrySport/NutrySportApplication.java
@@ -0,0 +1,13 @@
+package com.jmonteiro.nutrySport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrySportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrySportApplication.class, args);
+	}
+
+}
