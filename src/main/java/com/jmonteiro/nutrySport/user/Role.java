799f73db331873eec97a99514f45589155d5dce6
João MONTEIRO <jmonteiro@apsdigit.lan>
2023-03-01 16:28:13 +0000

    Enum role reacted, anda UserRepository created

---
+++ src/main/java/com/jmonteiro/nutrySport/user/Role.java
@@ -0,0 +1,6 @@
+package com.jmonteiro.nutrySport.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/jmonteiro/nutrySport/user/User.java
+++ src/main/java/com/jmonteiro/nutrySport/user/User.java
@@ -6,9 +6,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -24,35 +26,42 @@ public class User implements UserDetails {
     private String firstname;
     private String lastName;
     private String email;
-    private String password;
+    private String password ;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
