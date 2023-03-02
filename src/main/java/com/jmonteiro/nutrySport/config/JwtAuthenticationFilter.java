b11096c0cff6d96af2c0b7528c5f274f2f1863b3
João MONTEIRO <jmonteiro@apsdigit.lan>
2023-03-02 11:16:55 +0000

    test

--- src/main/java/com/jmonteiro/nutrySport/config/JwtAuthenticationFilter.java
+++ src/main/java/com/jmonteiro/nutrySport/config/JwtAuthenticationFilter.java
@@ -36,6 +36,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         userEmail = jwtService.extractUsername(jwt);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+
         }
     }
 }
